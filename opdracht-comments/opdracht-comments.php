335c309aa2c3dddd051e182b3d47edde67ab997c
Sophie Moons <moonssophie@gmail.com>
2015-09-30 10:33:54 +0200

    opdracht-comments

---
+++ opdracht-comments/opdracht-comments.php
@@ -0,0 +1,29 @@
+/*Sophie Moons
+moonssophie@gmail.com*/ 
+
+<?php
+	
+	$naam	="Sophie";
+	$achternaam="Moons";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Opdracht Comments()</title>
+</head>
+<body>
+
+	<section>
+
+		<h1>opdracht-comments</h1>
+		
+		<p><?php echo( $naam . '&nbsp'. $achternaam) ?></p> 
+		#nbsp = beter als spatie omdat de html soms ' ' gewoon negeert
+
+	</section>
+
+</body>
+</html>
