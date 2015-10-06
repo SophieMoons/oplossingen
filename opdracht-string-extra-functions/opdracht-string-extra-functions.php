f94d9549729f855018ab46d186a1524eb02d389a
Sophie Moons <moonssophie@gmail.com>
2015-10-06 12:16:45 +0200

    opdracht-string-extra-functies

---
+++ opdracht-string-extra-functions/opdracht-string-extra-functions.php
@@ -0,0 +1,47 @@
+<?php
+#deel 1.
+$fruit = 'kokosnoot';
+$aantalCharsFruit = strlen($fruit);
+$posFruit = strpos( $fruit,'o');
+
+#deel 2.
+$fruit2 = 'ananas';
+$posFruit2 = strrpos($fruit2,'a');
+$capitalFruit2 = strtoupper($fruit2);
+
+#deel 3.
+$lettertje = 'e';
+$cijfertje = 3;
+$langsteWoord ='zandzeepsodemineralenwatersteenstralen';
+$vervangLangsteWoord =  str_replace(($lettertje), $cijfertje,$langsteWoord);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>opdracht-string-extra-functions</title>
+</head>
+<body>
+
+	<section>
+
+		<h1>opdracht-string-extra-functions</h1>
+		
+		<h2>Deel 1.</h2>
+		<p><?php echo( $aantalCharsFruit) ?></p> 
+		<p><?php echo( $posFruit) ?></p> 
+
+		<h2>Deel 2.</h2>
+		<p><?php echo( $posFruit2) ?></p> 
+		<p><?php echo( $capitalFruit2) ?></p> 
+
+		<h2>Deel 3.</h2>
+		<p><?php echo($vervangLangsteWoord) ?></p> 
+
+	</section>
+
+</body>
+</html>
