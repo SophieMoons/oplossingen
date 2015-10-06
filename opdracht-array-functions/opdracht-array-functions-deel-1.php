297164c7dc5cbe9700e91190eb426b253f20c5b4
Sophie Moons <moonssophie@gmail.com>
2015-10-06 16:33:40 +0200

    opdracht-array-functions-deel-1

---
+++ opdracht-array-functions/opdracht-array-functions-deel-1.php
@@ -0,0 +1,39 @@
+<?php
+#Deel 1
+$dierenArr = array('Papegaai','Pieterman','Pinguïn','Pissebed','Platvoet','Poes');
+
+$aantalDieren = count($dierenArr);
+
+$teZoekenDier = 'Koe';
+
+$gevonden=false;
+
+if(in_array($teZoekenDier, $dierenArr)==true)
+{
+$gevonden=true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/directory.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/facade.css">
+</head>
+
+<body>
+	
+	<section class="body">
+		<h1>opdracht-array-basis</h1>
+		<h2>Deel 1</h2>
+		<p>Er zijn in totaal <?php echo ($aantalDieren)?> dieren</p>
+		<p>Het dier <?php echo ($teZoekenDier)?> is <?php echo ($gevonden)? "gevonden" : "niet gevonden" ?></p>
+
+	</section>
+
+</body>
+</html>
