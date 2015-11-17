56b5f9837a6eb0699e0b463f04df19944515755a
Sophie Moons <moonssophie@gmail.com>
2015-11-17 15:50:41 +0100

    Opdracht include-require:
    Opmerkingen:
    -> 3de dimensie array values afgaan (tag array)
    
    Esthetisch:
    -> rare positie artikels
    -> de footer is niet responsive met de onderkant van de window

---
+++ opdracht-include-require/index (Fail).php
@@ -0,0 +1,36 @@
+<?php
+$artikels = array(
+
+						array( 'title' => 'Acht internetreuzen eisen beperkingen NSA-spionage',
+								
+								'body' => 'Acht belangrijke technologiebedrijven, zoals Google, Apple en Facebook, hebben in een open brief aan de Amerikaanse president Barack Obama veranderingen geëist in de manier waarop de overheid aan spionage doet.',
+								'tags' => array('spionage')),
+
+						array( 'title' => 'Wilde weldoener stopt geld in brievenbussen',
+							   'body' => 'Bewoners van twee appartements­blokken in Koksijde-Bad hebben al drie dagen op rij geld in hun brievenbus aangetroffen. De politie van de zone Westkust is een onderzoek gestart.',
+							   'tags' => array('immobiliën')),
+
+						array('title' => 'Originele stukken Hergé geveild voor honderdduizenden euro’s',
+							  'body' => 'Twee originele stukken van Hergé zijn zondag voor honderdduizenden euro’s verkocht op een veiling die simultaan werd georganiseerd in Brussel en Parijs.',
+							  'tags' => array('Hergé', 'strips','moneeeeey'))
+					);
+
+$aantalArtikels = sizeof($artikels); //hoeveel arrays in de array?
+
+if (isset($_GET['artikel'])) //enkel wanneer er een artikel aanwezig is
+	{
+		$artikel = $artikels[$_GET['artikel']];
+	}
+
+
+	include 'view/header-partial.html';
+	if (!isset($_GET['artikel']))
+	{
+		include 'view/artikels.html';
+	}
+	else
+	{
+		include 'view/artikel.html';
+	}
+	include 'view/footer-partial.html';
+?>
